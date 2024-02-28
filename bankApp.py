dc048a70370298fde856e22f40ea9f2c0c26b66e
Philip Engineer Software <philip@macbook-air-masha.home>
2024-02-28 12:45:00 +0100

    Initial commit

---
+++ bankApp.py
@@ -0,0 +1,88 @@
+from usersData import UserData
+import json
+
+class BankApp:
+    def __init__(self, user_data):
+        self.user_data = user_data
+
+    def checkSaldo(self, username):
+        with open('database.json', 'r') as file:
+            data = json.load(file)
+            if username in data:
+                saldo = data[username].get("saldo")
+                print(f"Saldo użytkownika {username}: {saldo}")
+
+    def deposit(self):
+        amount = float(input("Podaj kwotę do wpłaty: "))
+        if amount <= 0:
+            print("Podano nieprawidłową kwotę.")
+            return
+
+        logged_user = self.user_data.logged_user
+        if logged_user:
+            with open('database.json', 'r+') as file:
+                data = json.load(file)
+                if logged_user in data:
+                    data[logged_user]['saldo'] += amount
+                    file.seek(0)
+                    json.dump(data, file)
+                    print(f"Wpłacono {amount} zł. Aktualne saldo: {data[logged_user]['saldo']} zł.")
+        else:
+            print("Błąd: Brak zalogowanego użytkownika.")
+
+    def withdraw(self):
+        amount = float(input("Podaj kwotę do wypłaty: "))
+        if amount <= 0:
+            print("Podano nieprawidłową kwotę.")
+            return
+
+        logged_user = self.user_data.logged_user
+        if logged_user:
+            with open('database.json', 'r+') as file:
+                data = json.load(file)
+                if logged_user in data:
+                    saldo = data[logged_user]['saldo']
+                    if amount > saldo:
+                        print("Nie masz wystarczających środków na koncie.")
+                        return
+                    data[logged_user]['saldo'] -= amount
+                    file.seek(0)
+                    json.dump(data, file)
+                    print(f"Wypłacono {amount} zł. Aktualne saldo: {data[logged_user]['saldo']} zł.")
+        else:
+            print("Błąd: Brak zalogowanego użytkownika.")
+
+
+def runner():
+    while True:
+        user_data = UserData()  # Tworzenie instancji klasy UserData
+        bank_app = BankApp(user_data)  # Tworzenie instancji klasy BankApp
+        start = input("Wybierz: 1)Logowanie lub 2)Rejestracja lub q) Wyjście: ")
+        if start == "1":
+            user_data.login()
+            if user_data.logged_user:
+                while True:
+                    action = input("Wybierz co chcesz zrobić: 1)Saldo, 2)Wpłać, 3)Wypłać lub q) Wyloguj: ")
+                    if action == "1":
+                        bank_app.checkSaldo(user_data.logged_user)  # Wywołanie metody checkSaldo z nazwą użytkownika
+                    elif action == "2":
+                        bank_app.deposit()
+                    elif action == "3":
+                        bank_app.withdraw()
+                    elif action == "q":
+                        break
+                    else:
+                        print("Nieprawidłowy wybór.")
+        elif start == "2":
+            user_data.registration()
+        elif start == "q":
+            print("Dziękujemy za skorzystanie z naszego systemu bankowego. Do zobaczenia!")
+            break
+        else:
+            print("Nieprawidłowy wybór.")
+
+
+
+
+if __name__ == "__main__":
+    runner()
---
+++ usersData.py
@@ -0,0 +1,67 @@
+import json
+import os
+
+
+class UserData:
+    def __init__(self):
+        self.users = {}  # Słownik przechowujący dane użytkowników
+        self.logged_user = None  # Zmienna przechowująca nazwę zalogowanego użytkownika
+        self.load_from_json()
+
+    def registration(self):
+        while True:
+            username = input("Podaj nazwę użytkownika (minimum 5 znaków): ").capitalize()
+            if len(username) < 5:
+                print("Nazwa użytkownika musi zawierać przynajmniej 5 znaków.")
+                continue
+            elif username in self.users:
+                print("Użytkownik już istnieje. Proszę wybrać inną nazwę.")
+                continue
+
+            while True:
+                password = input(
+                    "Podaj hasło (minimum 8 znaków, co najmniej 1 duża litera, 1 znak specjalny i 1 cyfra): ")
+                if not self.validate_password(password):
+                    print("Hasło nie spełnia wymagań. Spróbuj ponownie.")
+                    continue
+                else:
+                    break  # Przerwij pętlę, jeśli hasło jest poprawne
+
+            saldo = 0  # Domyślne saldo dla nowego użytkownika
+            self.users[username] = {"password": password, "saldo": saldo}
+            self.save_to_json()
+            print("Rejestracja zakończona pomyślnie.")
+            return True
+
+    def validate_password(self, password):
+        if len(password) < 8:
+            return False
+        if not any(char.isupper() for char in password):
+            return False
+        if not any(char.isdigit() for char in password):
+            return False
+        if not any(char in "!@#$%^&*()-_+=[]" for char in password):
+            return False
+        return True
+
+    def login(self):
+        while True:
+            username = input("Podaj nazwę użytkownika: ").capitalize()
+            password = input("Podaj hasło: ")
+            if username in self.users and self.users[username]["password"] == password:
+                self.logged_user = username  # Ustawienie nazwy zalogowanego użytkownika
+                print("Zalogowano pomyślnie.")
+                return True
+            else:
+                print("Błąd logowania. Sprawdź nazwę użytkownika i hasło.")
+
+    def load_from_json(self):
+        if os.path.exists('database.json') and os.path.getsize('database.json') > 0:
+            with open('database.json', 'r') as file:
+                self.users = json.load(file)
+
+    def save_to_json(self):
+        with open('database.json', 'w') as file:
+            json.dump(self.users, file)
+
+
